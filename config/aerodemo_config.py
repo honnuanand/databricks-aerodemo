bc8847060f808ed23b07c82eb1ebf4a46588f305
Anand Rao <anand@honnu.com>
2025-05-28 11:33:08 -0700

    renamed some files to match new folder structure. Also added the config folder to have all the key configs and I need to start wiring it now

--- config/aerodemo_config.py
+++ config/aerodemo_config.py
@@ -5,5 +5,7 @@ CONFIG = {
         "full_pipeline": "pipeline-id-here",
         "registration_pipeline": "pipeline-id-here"
     },
-    "pat_token": "dapi-XXXXXX"
+    "pat_token": "dapi-XXXXXX",
+    "databricks_instance": "https://e2-demo-field-eng.cloud.databricks.com",
+    "e2e_workflow_job_id": "864722071013094"  # Add this for E2E workflow
 }
